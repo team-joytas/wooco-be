73b39a2e11c04158228732d3ea159be669587c84
JiHongKim98 <kinjihong9598@gmail.com>
2024-12-06 23:07:28 +0900

    chore: 프로젝트 초기 설정 추가 (#1)

---
+++ build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+    kotlin("jvm")
+    kotlin("plugin.spring")
+    id("org.springframework.boot")
+    id("io.spring.dependency-management")
+}
+
+group = "${property("projectGroup")}"
+version = "${property("applicationVersion")}"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testImplementation("io.kotest:kotest-runner-junit5-jvm:${property("kotestVersion")}")
+    testImplementation("io.kotest:kotest-framework-datatest:${property("kotestVersion")}")
+    testImplementation("io.kotest:kotest-assertions-core:${property("kotestVersion")}")
+    testImplementation("io.kotest.extensions:kotest-extensions-spring:${property("kotestExtensionsVersion")}")
+    testImplementation("io.mockk:mockk:${property("mockkVersion")}")
+}
+
+kotlin {
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict")
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ src/test/kotlin/kr/wooco/woocobe/WoocoBeApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.wooco.woocobe
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WoocoBeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
