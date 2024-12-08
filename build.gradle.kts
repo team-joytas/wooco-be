3b68ac8ff79958562ef12ee05b604c84e4936fad
JiHongKim98 <kinjihong9598@gmail.com>
2024-12-09 01:00:55 +0900

    chore: 프로젝트 초기 레디스 설정 추가 (#1)

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,7 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("io.github.oshai:kotlin-logging-jvm:${property("kotlinLoggingVersion")}")
