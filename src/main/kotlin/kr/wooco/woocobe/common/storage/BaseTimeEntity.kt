63878811e82f15dc47764fba6e7447380ada8fed
JiHongKim98 <kinjihong9598@gmail.com>
2024-12-10 20:48:25 +0900

    feat: 기본 엔티티 클래스 추가 (#3)

---
+++ src/main/kotlin/kr/wooco/woocobe/common/storage/BaseTimeEntity.kt
@@ -0,0 +1,22 @@
+package kr.wooco.woocobe.common.storage
+
+import jakarta.persistence.Column
+import jakarta.persistence.EntityListeners
+import jakarta.persistence.MappedSuperclass
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.time.LocalDateTime
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener::class)
+class BaseTimeEntity(
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    var createdAt: LocalDateTime = LocalDateTime.now(),
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    var updatedAt: LocalDateTime = LocalDateTime.now(),
+)
