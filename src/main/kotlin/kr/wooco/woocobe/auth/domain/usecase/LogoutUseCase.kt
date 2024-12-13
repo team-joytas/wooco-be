3a831936724a7579994dd89c399590e18a3a3d5a
JiHongKim98 <kinjihong9598@gmail.com>
2024-12-13 11:01:12 +0900

    feat: 로그아웃 유즈 케이스 기능 추가 (#11)

---
+++ src/main/kotlin/kr/wooco/woocobe/auth/domain/usecase/LogoutUseCase.kt
@@ -0,0 +1,27 @@
+package kr.wooco.woocobe.auth.domain.usecase
+
+import kr.wooco.woocobe.auth.domain.gateway.AuthTokenStorageGateway
+import kr.wooco.woocobe.auth.domain.gateway.TokenProviderGateway
+import kr.wooco.woocobe.common.domain.UseCase
+import org.springframework.stereotype.Service
+
+data class LogoutInput(
+    val userId: Long,
+    val refreshToken: String,
+)
+
+@Service
+class LogoutUseCase(
+    private val tokenProviderGateway: TokenProviderGateway,
+    private val authTokenStorageGateway: AuthTokenStorageGateway,
+) : UseCase<LogoutInput, Unit> {
+    override fun execute(input: LogoutInput) {
+        val tokenId = tokenProviderGateway.extractRefreshToken(input.refreshToken)
+        val authToken = authTokenStorageGateway.getByTokenId(tokenId)
+            ?: throw RuntimeException()
+
+        when {
+            authToken.isMatchUserId(input.userId).not() -> throw RuntimeException()
+        }
+    }
+}
