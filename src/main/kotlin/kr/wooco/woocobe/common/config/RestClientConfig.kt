44d231718875d2fe65ada69fec81120df5200f2d
JiHongKim98 <kinjihong9598@gmail.com>
2024-12-13 10:46:40 +0900

    feat: 카카오 OAuth 클라이언트 기능 추가 (#11)

---
+++ src/main/kotlin/kr/wooco/woocobe/common/config/RestClientConfig.kt
@@ -0,0 +1,11 @@
+package kr.wooco.woocobe.common.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.client.RestClient
+
+@Configuration
+class RestClientConfig {
+    @Bean
+    fun restClient(): RestClient = RestClient.create()
+}
