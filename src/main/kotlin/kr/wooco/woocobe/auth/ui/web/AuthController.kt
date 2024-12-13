31d7c4888b65b6dc180d0cc1b2e63f74164f2074
JiHongKim98 <kinjihong9598@gmail.com>
2024-12-13 11:02:58 +0900

    feat: 인증 도메인 ui 레이어 추가 (#11)

---
+++ src/main/kotlin/kr/wooco/woocobe/auth/ui/web/AuthController.kt
@@ -0,0 +1,10 @@
+package kr.wooco.woocobe.auth.ui.web
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/v1/auth")
+class AuthController
+
+// TODO
