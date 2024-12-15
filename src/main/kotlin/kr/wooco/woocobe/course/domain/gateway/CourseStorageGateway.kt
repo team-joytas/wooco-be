89536a19ef2378bcb19147b277a7a849537dd7ff
JiHongKim98 <kinjihong9598@gmail.com>
2024-12-16 01:37:28 +0900

    feat: 코스 도메인 저장 게이트웨이 로직 추가 (#22)

--- src/main/kotlin/kr/wooco/woocobe/course/domain/gateway/CourseStorageGateway.kt
+++ src/main/kotlin/kr/wooco/woocobe/course/domain/gateway/CourseStorageGateway.kt
@@ -1,9 +1,24 @@
 package kr.wooco.woocobe.course.domain.gateway
 
 import kr.wooco.woocobe.course.domain.model.Course
+import kr.wooco.woocobe.course.domain.model.CourseRegion
+import kr.wooco.woocobe.course.domain.model.CourseSortCondition
 
 interface CourseStorageGateway {
     fun save(course: Course): Course
 
     fun getByCourseId(courseId: Long): Course?
+
+    fun getAllByRegionAndCategoryWithSort(
+        region: CourseRegion,
+        category: String,
+        sort: CourseSortCondition,
+    ): List<Course>
+
+    fun getAllByUserIdWithSort(
+        userId: Long,
+        sort: CourseSortCondition,
+    ): List<Course>
+
+    fun deleteByCourseId(courseId: Long)
 }
