bfcf892e82b50c7421904ccd11344171b96ea8dc
jspark <98972385+JunseoParKK@users.noreply.github.com>
2024-12-15 21:26:48 +0900

    refactor: 플랜 도메인에 유저 도메인 의존성 추가 (#15)

--- src/main/kotlin/kr/wooco/woocobe/plan/domain/model/Plan.kt
+++ src/main/kotlin/kr/wooco/woocobe/plan/domain/model/Plan.kt
@@ -1,26 +1,40 @@
 package kr.wooco.woocobe.plan.domain.model
 
+import kr.wooco.woocobe.common.domain.IdGenerator
+import kr.wooco.woocobe.user.domain.model.User
 import java.time.LocalDate
 
 // TODO: 장소 정보 추가
 class Plan(
     val id: Long,
-    val userId: Long,
+    val writer: User,
     val regionInfo: PlanRegionInfo,
     val visitDate: LocalDate,
 ) {
     companion object {
         fun register(
-            planId: Long,
-            userId: Long,
+            user: User,
             regionInfo: PlanRegionInfo,
             visitDate: LocalDate,
         ): Plan =
             Plan(
-                id = planId,
-                userId = userId,
+                id = IdGenerator.generateId(),
+                writer = user,
                 regionInfo = regionInfo,
                 visitDate = visitDate,
             )
     }
+
+    fun isWriter(writerId: Long): Boolean = writerId == writer.id
+
+    fun withUpdatedValues(
+        regionInfo: PlanRegionInfo?,
+        visitDate: LocalDate?,
+    ): Plan =
+        Plan(
+            id = this.id,
+            writer = this.writer,
+            regionInfo = regionInfo ?: this.regionInfo,
+            visitDate = visitDate ?: this.visitDate,
+        )
 }
