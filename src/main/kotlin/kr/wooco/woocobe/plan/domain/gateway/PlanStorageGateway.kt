3d621c86602d9d9cacb2c5b4db86bce32642ebf8
jspark <98972385+JunseoParKK@users.noreply.github.com>
2024-12-16 21:29:24 +0900

    feat: 플랜 저장소 게이트웨이 및 구현체 기능 추가 (#15)

--- src/main/kotlin/kr/wooco/woocobe/plan/domain/gateway/PlanStorageGateway.kt
+++ src/main/kotlin/kr/wooco/woocobe/plan/domain/gateway/PlanStorageGateway.kt
@@ -4,4 +4,10 @@ import kr.wooco.woocobe.plan.domain.model.Plan
 
 interface PlanStorageGateway {
     fun save(plan: Plan): Plan
+
+    fun getById(id: Long): Plan?
+
+    fun getAllByUserId(userId: Long): List<Plan>?
+
+    fun deleteById(id: Long)
 }
--- src/main/kotlin/kr/wooco/woocobe/plan/infrastructure/gateway/JpaPlanStorageGateway.kt
+++ src/main/kotlin/kr/wooco/woocobe/plan/infrastructure/gateway/JpaPlanStorageGateway.kt
@@ -4,11 +4,20 @@ import kr.wooco.woocobe.plan.domain.gateway.PlanStorageGateway
 import kr.wooco.woocobe.plan.domain.model.Plan
 import kr.wooco.woocobe.plan.infrastructure.storage.PlanEntity
 import kr.wooco.woocobe.plan.infrastructure.storage.PlanJpaRepository
-import org.springframework.stereotype.Repository
+import org.springframework.stereotype.Component
 
-@Repository
+@Component
 class JpaPlanStorageGateway(
     private val planJpaRepository: PlanJpaRepository,
 ) : PlanStorageGateway {
     override fun save(plan: Plan): Plan = planJpaRepository.save(PlanEntity.from(plan)).toDomain()
+
+    override fun getById(id: Long): Plan? = planJpaRepository.findByIdWithUser(id)?.toDomain()
+
+    override fun getAllByUserId(userId: Long): List<Plan>? =
+        planJpaRepository
+            .findAllByUserId(userId)
+            ?.map { it.toDomain() }
+
+    override fun deleteById(id: Long) = planJpaRepository.deleteById(id)
 }
