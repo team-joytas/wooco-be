6ff8f369f56adfb1cdc0a6744626b411e19cb5b1
JiHongKim98 <kinjihong9598@gmail.com>
2024-12-16 15:43:28 +0900

    feat: 코스 수정 유즈 케이스 추가 (#22)

---
+++ src/main/kotlin/kr/wooco/woocobe/course/domain/usecase/UpdateCourseUseCase.kt
@@ -0,0 +1,36 @@
+package kr.wooco.woocobe.course.domain.usecase
+
+import kr.wooco.woocobe.common.domain.UseCase
+import kr.wooco.woocobe.course.domain.gateway.CourseStorageGateway
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+data class UpdateCourseInput(
+    val userId: Long,
+    val courseId: Long,
+    val name: String,
+    val contents: String,
+    val categories: List<String>,
+)
+
+@Service
+class UpdateCourseUseCase(
+    private val courseStorageGateway: CourseStorageGateway,
+) : UseCase<UpdateCourseInput, Unit> {
+    @Transactional
+    override fun execute(input: UpdateCourseInput) {
+        val course = courseStorageGateway.getByCourseId(courseId = input.courseId)
+            ?: throw RuntimeException()
+
+        when {
+            course.isWriter(input.userId).not() -> throw RuntimeException()
+        }
+
+        course
+            .update(
+                name = input.name,
+                contents = input.contents,
+                categories = input.categories,
+            ).also(courseStorageGateway::save)
+    }
+}
