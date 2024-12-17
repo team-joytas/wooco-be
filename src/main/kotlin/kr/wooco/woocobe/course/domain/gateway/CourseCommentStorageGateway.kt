22e6b8fa5e266fbcd20a86e48faacd577ca6bf64
JiHongKim98 <kinjihong9598@gmail.com>
2024-12-17 23:01:18 +0900

    feat: 코스 댓글 게이트웨이 추가 (#28)

---
+++ src/main/kotlin/kr/wooco/woocobe/course/domain/gateway/CourseCommentStorageGateway.kt
@@ -0,0 +1,13 @@
+package kr.wooco.woocobe.course.domain.gateway
+
+import kr.wooco.woocobe.course.domain.model.CourseComment
+
+interface CourseCommentStorageGateway {
+    fun save(courseComment: CourseComment): CourseComment
+
+    fun getByCommentId(commentId: Long): CourseComment?
+
+    fun getAllByCourseId(courseId: Long): List<CourseComment>
+
+    fun deleteByCommentId(commentId: Long)
+}
