c2e627fe77ef2dcd1c8b4704dfbb2b32f0c55cd2
JiHongKim98 <kinjihong9598@gmail.com>
2024-12-17 23:58:11 +0900

    feat: 코스 댓글 삭제 유즈 케이스 추가 (#28)

---
+++ src/main/kotlin/kr/wooco/woocobe/course/domain/usecase/DeleteCourseCommentUseCase.kt
@@ -0,0 +1,36 @@
+package kr.wooco.woocobe.course.domain.usecase
+
+import kr.wooco.woocobe.common.domain.UseCase
+import kr.wooco.woocobe.course.domain.gateway.CourseCommentStorageGateway
+import kr.wooco.woocobe.course.domain.gateway.CourseStorageGateway
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+data class DeleteCourseCommentInput(
+    val userId: Long,
+    val commentId: Long,
+)
+
+@Service
+class DeleteCourseCommentUseCase(
+    private val courseStorageGateway: CourseStorageGateway,
+    private val courseCommentStorageGateway: CourseCommentStorageGateway,
+) : UseCase<DeleteCourseCommentInput, Unit> {
+    @Transactional
+    override fun execute(input: DeleteCourseCommentInput) {
+        val courseComment = courseCommentStorageGateway
+            .getByCommentId(input.commentId)
+            ?: throw RuntimeException()
+
+        when {
+            courseComment.isCommenter(input.commentId).not() -> throw RuntimeException()
+        }
+
+        courseCommentStorageGateway.deleteByCommentId(commentId = courseComment.id)
+
+        courseStorageGateway
+            .getByCourseId(courseComment.courseId)
+            ?.run { decreaseComments().also(courseStorageGateway::save) }
+            ?: throw RuntimeException()
+    }
+}
