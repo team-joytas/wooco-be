e4f42b0a8a720dc03acf9dc59fe3fcfc630a5f7e
jspark <98972385+JunseoParKK@users.noreply.github.com>
2024-12-18 19:37:00 +0900

    refactor: GetAllPlanResponse 클래스명 및 파라미터 형식 변경 (#15)

---
+++ src/main/kotlin/kr/wooco/woocobe/plan/ui/web/dto/response/GetAllPlanResponse.kt
@@ -0,0 +1,23 @@
+package kr.wooco.woocobe.plan.ui.web.dto.response
+
+import kr.wooco.woocobe.plan.domain.usecase.GetAllPlanOutput
+
+data class GetAllPlanResponse(
+    val plans: List<GetPlanResponse>,
+) {
+    companion object {
+        fun from(getAllPlanOutput: GetAllPlanOutput): GetAllPlanResponse =
+            GetAllPlanResponse(
+                getAllPlanOutput.plans.map { plan ->
+                    GetPlanResponse(
+                        planId = plan.id,
+                        userId = plan.user.id,
+                        userName = plan.user.name,
+                        primaryRegion = plan.region.primaryRegion,
+                        secondaryRegion = plan.region.secondaryRegion,
+                        visitDate = plan.visitDate.toString(),
+                    )
+                },
+            )
+    }
+}
