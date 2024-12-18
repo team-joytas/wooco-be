7819c7b17a35404e86d32e27b4fe2f57a5108bd8
jspark <98972385+JunseoParKK@users.noreply.github.com>
2024-12-18 19:39:41 +0900

    refactor: GetPlanResponse 파라미터 형식 변경 (#15)

--- src/main/kotlin/kr/wooco/woocobe/plan/ui/web/dto/response/GetPlanResponse.kt
+++ src/main/kotlin/kr/wooco/woocobe/plan/ui/web/dto/response/GetPlanResponse.kt
@@ -5,22 +5,24 @@ import kr.wooco.woocobe.plan.domain.usecase.GetPlanOutput
 // TODO: 장소 데이터 추가
 data class GetPlanResponse(
     val planId: Long,
-    val writerId: Long,
-    val writerName: String,
+    val userId: Long,
+    val userName: String,
     val primaryRegion: String,
     val secondaryRegion: String,
     val visitDate: String,
     // val places: List<Place>
 ) {
     companion object {
-        fun from(plan: GetPlanOutput): GetPlanResponse =
-            GetPlanResponse(
-                plan.id,
-                plan.writerId,
-                plan.writerName,
-                plan.primaryRegion,
-                plan.secondaryRegion,
-                plan.visitDate,
-            )
+        fun from(getPlanOutput: GetPlanOutput) =
+            with(getPlanOutput.plan) {
+                GetPlanResponse(
+                    planId = id,
+                    userId = user.id,
+                    userName = user.name,
+                    primaryRegion = region.primaryRegion,
+                    secondaryRegion = region.secondaryRegion,
+                    visitDate = visitDate.toString(),
+                )
+            }
     }
 }
