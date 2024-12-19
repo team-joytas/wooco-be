1b1874e542272270492304d023b3f2aca6067020
jspark <98972385+JunseoParKK@users.noreply.github.com>
2024-12-19 20:18:35 +0900

    refactor: 불필요한 플랜 조회 쿼리 및 메서드 제거 (#30)

--- src/main/kotlin/kr/wooco/woocobe/plan/infrastructure/storage/PlanJpaRepository.kt
+++ src/main/kotlin/kr/wooco/woocobe/plan/infrastructure/storage/PlanJpaRepository.kt
@@ -4,9 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 
 interface PlanJpaRepository : JpaRepository<PlanEntity, Long> {
-    @Query("SELECT p FROM PlanEntity p LEFT JOIN FETCH p.user WHERE p.id = :id")
-    fun findByIdWithUser(id: Long): PlanEntity?
-
-    @Query("SELECT p FROM PlanEntity p LEFT JOIN FETCH p.user WHERE p.user.id = :userId")
+    @Query("SELECT p FROM PlanEntity p WHERE p.userId = :userId")
     fun findAllByUserId(userId: Long): List<PlanEntity>
 }
