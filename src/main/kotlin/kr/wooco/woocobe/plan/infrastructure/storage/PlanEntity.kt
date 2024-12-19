8091e7adcd2c6146e2df49e0d5421a9375dd0aac
jspark <98972385+JunseoParKK@users.noreply.github.com>
2024-12-19 19:59:55 +0900

    refactor: 플랜 엔티티에서 유저 엔티티 연관 관계 제거 (#30)

--- src/main/kotlin/kr/wooco/woocobe/plan/infrastructure/storage/PlanEntity.kt
+++ src/main/kotlin/kr/wooco/woocobe/plan/infrastructure/storage/PlanEntity.kt
@@ -1,39 +1,35 @@
 package kr.wooco.woocobe.plan.infrastructure.storage
 
+import io.hypersistence.utils.hibernate.id.Tsid
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
-import jakarta.persistence.FetchType
 import jakarta.persistence.Id
-import jakarta.persistence.JoinColumn
-import jakarta.persistence.ManyToOne
 import jakarta.persistence.Table
 import kr.wooco.woocobe.common.storage.BaseTimeEntity
 import kr.wooco.woocobe.plan.domain.model.Plan
 import kr.wooco.woocobe.plan.domain.model.PlanDate
 import kr.wooco.woocobe.plan.domain.model.PlanRegion
-import kr.wooco.woocobe.user.infrastructure.storage.UserEntity
 import java.time.LocalDate
 
 @Entity
 @Table(name = "plans")
 class PlanEntity(
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
-    val user: UserEntity,
+    @Column(name = "user_id")
+    val userId: Long,
     @Column(name = "primary_region")
     val primaryRegion: String,
     @Column(name = "secondary_region")
     val secondaryRegion: String,
     @Column(name = "visit_date")
     val visitDate: LocalDate,
-    @Id
+    @Id @Tsid
     @Column(name = "plan_id")
-    val id: Long,
+    val id: Long? = 0L,
 ) : BaseTimeEntity() {
     fun toDomain(): Plan =
         Plan(
-            id = id,
-            user = user.toDomain(),
+            id = id!!,
+            userId = userId,
             region = PlanRegion.register(
                 primaryRegion = primaryRegion,
                 secondaryRegion = secondaryRegion,
@@ -46,7 +42,7 @@ class PlanEntity(
             with(plan) {
                 PlanEntity(
                     id = id,
-                    user = UserEntity.from(plan.user),
+                    userId = userId,
                     primaryRegion = region.primaryRegion,
                     secondaryRegion = region.secondaryRegion,
                     visitDate = visitDate.date,
