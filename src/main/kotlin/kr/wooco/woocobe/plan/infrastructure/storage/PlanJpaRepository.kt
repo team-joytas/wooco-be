1b6b247346159b8f56408fa9c3021176dbda6a62
jspark <98972385+JunseoParKK@users.noreply.github.com>
2024-12-19 20:52:35 +0900

    refactor: 불필요한 @Query 어노테이션 및 쿼리문 제거 (#30)

--- src/main/kotlin/kr/wooco/woocobe/plan/infrastructure/storage/PlanJpaRepository.kt
+++ src/main/kotlin/kr/wooco/woocobe/plan/infrastructure/storage/PlanJpaRepository.kt
@@ -1,9 +1,7 @@
 package kr.wooco.woocobe.plan.infrastructure.storage
 
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Query
 
 interface PlanJpaRepository : JpaRepository<PlanEntity, Long> {
-    @Query("SELECT p FROM PlanEntity p WHERE p.userId = :userId")
     fun findAllByUserId(userId: Long): List<PlanEntity>
 }
