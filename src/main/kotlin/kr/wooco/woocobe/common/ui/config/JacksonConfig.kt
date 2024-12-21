2ed5f8a59c01734dc6062e47bb9860d674f6f48b
JiHongKim98 <kinjihong9598@gmail.com>
2024-12-21 13:17:55 +0900

    feat: 전역 jackson 설정 추가 (#38)

---
+++ src/main/kotlin/kr/wooco/woocobe/common/ui/config/JacksonConfig.kt
@@ -0,0 +1,22 @@
+package kr.wooco.woocobe.common.ui.config
+
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.PropertyNamingStrategies
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class JacksonConfig {
+    @Bean
+    fun objectMapper(): ObjectMapper =
+        jacksonObjectMapper().apply {
+            propertyNamingStrategy = PropertyNamingStrategies.SNAKE_CASE
+            registerModule(JavaTimeModule())
+            disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+            configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+        }
+}
