d2a8911ebf6abea939e518f4e1ff8922e2858fe2
JiHongKim98 <kinjihong9598@gmail.com>
2024-12-21 15:29:33 +0900

    refactor: 전역 예외 API 응답 형식 수정 (#38)

--- src/main/kotlin/kr/wooco/woocobe/common/domain/exception/BaseErrorCode.kt
+++ src/main/kotlin/kr/wooco/woocobe/common/domain/exception/BaseErrorCode.kt
@@ -3,12 +3,13 @@ package kr.wooco.woocobe.common.domain.exception
 import org.springframework.http.HttpStatus
 
 enum class BaseErrorCode(
-    val code: String,
     val message: String,
     val status: HttpStatus,
 ) {
-    METHOD_ARGUMENT_TYPE_MISMATCH("METHOD_ARGUMENT_TYPE_MISMATCH", "method argument error", HttpStatus.BAD_REQUEST),
-    METHOD_NOT_SUPPORT("METHOD_NOT_SUPPORTED", "method not supported", HttpStatus.METHOD_NOT_ALLOWED),
-    NO_RESOURCE("NO_RESOURCE", "no such resource", HttpStatus.NOT_FOUND),
-    UNKNOWN("UN_KNOWN_ERROR", "not included in any exception class", HttpStatus.INTERNAL_SERVER_ERROR),
+    INVALID_REQUEST_ERROR("missing request body or request parameters", HttpStatus.BAD_REQUEST),
+    AUTHENTICATION_ERROR("authentication failed", HttpStatus.UNAUTHORIZED),
+    ACCESS_DENIED_ERROR_CODE("access denied", HttpStatus.FORBIDDEN),
+    METHOD_NOT_SUPPORT_ERROR("method not supported", HttpStatus.METHOD_NOT_ALLOWED),
+    NO_RESOURCE_ERROR("no such resource", HttpStatus.NOT_FOUND),
+    UNKNOWN_ERROR("not included in any exception class", HttpStatus.INTERNAL_SERVER_ERROR),
 }
