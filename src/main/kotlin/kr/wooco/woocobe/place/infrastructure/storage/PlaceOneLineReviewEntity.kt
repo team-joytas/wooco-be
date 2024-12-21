6985a09913712a399fc7fee9a6c297c13851d39d
Namgyu11 <103015031+Namgyu11@users.noreply.github.com>
2024-12-21 13:33:19 +0900

    feat: 장소 리뷰 저장소 및 게이트웨이 구현 추가 (#23)

---
+++ src/main/kotlin/kr/wooco/woocobe/place/infrastructure/storage/PlaceOneLineReviewEntity.kt
@@ -0,0 +1,34 @@
+package kr.wooco.woocobe.place.infrastructure.storage
+
+import io.hypersistence.utils.hibernate.id.Tsid
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import kr.wooco.woocobe.common.storage.BaseTimeEntity
+import kr.wooco.woocobe.place.domain.model.PlaceOneLineReview
+
+@Entity
+@Table(name = "place_one_line_reviews")
+class PlaceOneLineReviewEntity(
+    @Column(name = "content")
+    val content: String,
+    @Column(name = "place_id")
+    val placeReviewId: Long,
+    @Id @Tsid
+    @Column(name = "place_one_line_review_id")
+    val id: Long? = 0L,
+) : BaseTimeEntity() {
+    fun toDomain(): PlaceOneLineReview = PlaceOneLineReview(content = content)
+
+    companion object {
+        fun of(
+            placeReviewId: Long,
+            content: String,
+        ): PlaceOneLineReviewEntity =
+            PlaceOneLineReviewEntity(
+                placeReviewId = placeReviewId,
+                content = content,
+            )
+    }
+}
