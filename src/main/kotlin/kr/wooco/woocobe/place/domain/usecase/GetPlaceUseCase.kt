16e4be8c13a51f829ee58094c8f4421fb415061b
Namgyu11 <103015031+Namgyu11@users.noreply.github.com>
2024-12-21 13:26:46 +0900

    feat: 장소 조회 UseCase 추가 (#23)

---
+++ src/main/kotlin/kr/wooco/woocobe/place/domain/usecase/GetPlaceUseCase.kt
@@ -0,0 +1,29 @@
+package kr.wooco.woocobe.place.domain.usecase
+
+import kr.wooco.woocobe.common.domain.UseCase
+import kr.wooco.woocobe.place.domain.gateway.PlaceStorageGateway
+import kr.wooco.woocobe.place.domain.model.Place
+import org.springframework.stereotype.Service
+
+data class GetPlaceInput(
+    val userId: Long?,
+    val placeId: Long,
+)
+
+data class GetPlaceOutPut(
+    val place: Place,
+)
+
+@Service
+class GetPlaceUseCase(
+    private val placeStorageGateway: PlaceStorageGateway,
+) : UseCase<GetPlaceInput, GetPlaceOutPut> {
+    override fun execute(input: GetPlaceInput): GetPlaceOutPut {
+        val place = placeStorageGateway.getByPlaceId(input.placeId)
+            ?: throw RuntimeException()
+
+        return GetPlaceOutPut(
+            place = place,
+        )
+    }
+}
