545acd307b0431695287d40681082bb60bfb4c34
JiHongKim98 <kinjihong9598@gmail.com>
2024-12-26 17:31:42 +0900

    feat: 필터에서 발생한 예외 전역 처리 핸들러 추가 (#51)

--- src/main/kotlin/kr/wooco/woocobe/common/ui/config/WebFilterConfig.kt
+++ src/main/kotlin/kr/wooco/woocobe/common/ui/config/WebFilterConfig.kt
@@ -1,19 +1,29 @@
 package kr.wooco.woocobe.common.ui.config
 
+import kr.wooco.woocobe.common.ui.filter.ExceptionResolverFilter
 import kr.wooco.woocobe.common.ui.filter.MdcLoggingFilter
 import kr.wooco.woocobe.common.ui.filter.ReqResLoggingFilter
 import org.springframework.boot.web.servlet.FilterRegistrationBean
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.Ordered
+import org.springframework.web.servlet.HandlerExceptionResolver
 
 @Configuration
 class WebFilterConfig {
     @Bean
+    fun exceptionResolverFilter(handlerExceptionResolver: HandlerExceptionResolver): FilterRegistrationBean<ExceptionResolverFilter> =
+        FilterRegistrationBean<ExceptionResolverFilter>().apply {
+            filter = ExceptionResolverFilter(handlerExceptionResolver)
+            order = Ordered.HIGHEST_PRECEDENCE
+            addUrlPatterns("/*")
+        }
+
+    @Bean
     fun mdcLoggingFilter(): FilterRegistrationBean<MdcLoggingFilter> =
         FilterRegistrationBean<MdcLoggingFilter>().apply {
             filter = MdcLoggingFilter()
-            order = Ordered.HIGHEST_PRECEDENCE
+            order = Ordered.HIGHEST_PRECEDENCE + 1
             addUrlPatterns("/*")
         }
 
@@ -21,7 +31,7 @@ class WebFilterConfig {
     fun reqResLoggingFilter(): FilterRegistrationBean<ReqResLoggingFilter> =
         FilterRegistrationBean<ReqResLoggingFilter>().apply {
             filter = ReqResLoggingFilter()
-            order = Ordered.HIGHEST_PRECEDENCE + 1
+            order = Ordered.HIGHEST_PRECEDENCE + 2
             addUrlPatterns("/*")
         }
 }
