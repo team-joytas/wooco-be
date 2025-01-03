b3c291a2b39bf8780231ae206b8e23691bac67ea
jspark <98972385+JunseoParKK@users.noreply.github.com>
2025-01-03 16:14:39 +0900

    feat: 플랜 생성시 필요한 정보 추가 (#15)

--- src/main/kotlin/kr/wooco/woocobe/plan/domain/usecase/AddPlanUseCase.kt
+++ src/main/kotlin/kr/wooco/woocobe/plan/domain/usecase/AddPlanUseCase.kt
@@ -10,9 +10,13 @@ import java.time.LocalDate
 
 data class AddPlanInput(
     val userId: Long,
+    val title: String,
+    val description: String,
     val primaryRegion: String,
     val secondaryRegion: String,
     val visitDate: LocalDate,
+    val placeIds: List<Long>,
+    val categories: List<String>,
 )
 
 @Service
@@ -21,16 +25,20 @@ class AddPlanUseCase(
 ) : UseCase<AddPlanInput, Unit> {
     @Transactional
     override fun execute(input: AddPlanInput) {
-        val region = PlanRegion.register(
+        val region = PlanRegion(
             primaryRegion = input.primaryRegion,
             secondaryRegion = input.secondaryRegion,
         )
 
-        Plan
-            .register(
-                userId = input.userId,
-                region = region,
-                visitDate = input.visitDate,
-            ).also(planStorageGateway::save)
+        val plan = Plan.register(
+            userId = input.userId,
+            title = input.title,
+            description = input.description,
+            region = region,
+            visitDate = input.visitDate,
+            placeIds = input.placeIds,
+            categories = input.categories,
+        )
+        planStorageGateway.save(plan)
     }
 }
