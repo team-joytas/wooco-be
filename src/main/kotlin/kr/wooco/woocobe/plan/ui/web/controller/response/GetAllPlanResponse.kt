2716e8686309eca1ec230f96bf9ab4ecf7269fc1
jspark <98972385+JunseoParKK@users.noreply.github.com>
2025-01-07 16:23:56 +0900

    refactor: 플랜 단일 및 목록 조회 응답 dto 클래스 형식 수정 (#15)

--- src/main/kotlin/kr/wooco/woocobe/plan/ui/web/controller/response/GetAllPlanResponse.kt
+++ src/main/kotlin/kr/wooco/woocobe/plan/ui/web/controller/response/GetAllPlanResponse.kt
@@ -1,22 +1,34 @@
-package kr.wooco.woocobe.plan.ui.web.dto.response
+package kr.wooco.woocobe.plan.ui.web.controller.response
 
+import kr.wooco.woocobe.plan.domain.model.PlanPlace
 import kr.wooco.woocobe.plan.domain.usecase.GetAllPlanOutput
+import java.time.LocalDate
 
 data class GetAllPlanResponse(
-    val plans: List<GetPlanResponse>,
+    val plans: List<SimplePlanResponse>,
 ) {
     companion object {
         fun from(getAllPlanOutput: GetAllPlanOutput): GetAllPlanResponse =
             GetAllPlanResponse(
                 getAllPlanOutput.plans.map { plan ->
-                    GetPlanResponse(
+                    SimplePlanResponse(
                         planId = plan.id,
-                        userId = plan.userId,
+                        title = plan.title,
                         primaryRegion = plan.region.primaryRegion,
                         secondaryRegion = plan.region.secondaryRegion,
                         visitDate = plan.visitDate,
+                        places = plan.places,
                     )
                 },
             )
     }
 }
+
+data class SimplePlanResponse(
+    val planId: Long,
+    val title: String,
+    val primaryRegion: String,
+    val secondaryRegion: String,
+    val visitDate: LocalDate,
+    val places: List<PlanPlace>,
+)
