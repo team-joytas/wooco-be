17258f008ef1fd3a996eb62171ef549cfc39b015
Namgyu11 <103015031+Namgyu11@users.noreply.github.com>
2025-01-09 22:35:27 +0900

    style: 클래스 이름에서 'Jpa' 접두사 제거 (#70)

--- src/main/kotlin/kr/wooco/woocobe/place/infrastructure/gateway/PlaceReviewStorageGatewayImpl.kt
+++ src/main/kotlin/kr/wooco/woocobe/place/infrastructure/gateway/PlaceReviewStorageGatewayImpl.kt
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Component
 
 @Component
 @Suppress("Duplicates")
-class JpaPlaceReviewStorageGatewayImpl(
+class PlaceReviewStorageGatewayImpl(
     private val placeReviewJpaRepository: PlaceReviewJpaRepository,
     private val placeReviewImageJpaRepository: PlaceReviewImageJpaRepository,
     private val placeOneLineReviewJpaRepository: PlaceOneLineReviewJpaRepository,
--- src/main/kotlin/kr/wooco/woocobe/place/infrastructure/gateway/PlaceStorageGatewayImpl.kt
+++ src/main/kotlin/kr/wooco/woocobe/place/infrastructure/gateway/PlaceStorageGatewayImpl.kt
@@ -11,7 +11,7 @@ import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Component
 
 @Component
-class JpaPlaceStorageGatewayImpl(
+class PlaceStorageGatewayImpl(
     private val placeJpaRepository: PlaceJpaRepository,
     private val placeOneLineReviewRepository: PlaceOneLineReviewJpaRepository,
     private val placeStorageMapper: PlaceStorageMapper,
