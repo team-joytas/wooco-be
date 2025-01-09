6aca257c6bdd4a664b4286c170059620d82f4a62
Namgyu11 <103015031+Namgyu11@users.noreply.github.com>
2025-01-09 14:43:00 +0900

    refactor: `JpaPlaceStorageGateway` 클래스명 변경 및 매퍼 클래스 도입 (#70)

---
+++ src/main/kotlin/kr/wooco/woocobe/place/infrastructure/PlaceReviewStorageMapper.kt
@@ -0,0 +1,19 @@
+// package kr.wooco.woocobe.place.infrastructure
+//
+// import org.springframework.stereotype.Component
+//
+// @Component
+// class PlaceReviewStorageMapper{
+//    fun toDomain(
+//        placeReviewJpaEntity: PlaceReviewJpaEntity
+//    ): PlaceReview =
+//        PlaceReview(
+//            id = placeReviewJpaEntity.id!!,
+//            userId = placeReviewJpaEntity.userId,
+//            placeId = placeReviewJpaEntity.placeId,
+//            rating = placeReviewJpaEntity.rating,
+//            content = placeReviewJpaEntity.content,
+//            writeDateTime = placeReviewJpaEntity.createdAt,
+//        )
+//    )
+// }
---
+++ src/main/kotlin/kr/wooco/woocobe/place/infrastructure/PlaceStorageMapper.kt
@@ -0,0 +1,32 @@
+package kr.wooco.woocobe.place.infrastructure
+
+import kr.wooco.woocobe.place.domain.model.Place
+import kr.wooco.woocobe.place.infrastructure.storage.entity.PlaceJpaEntity
+import org.springframework.stereotype.Component
+
+@Component
+class PlaceStorageMapper {
+    fun toDomain(placeJpaEntity: PlaceJpaEntity): Place =
+        Place(
+            id = placeJpaEntity.id!!,
+            name = placeJpaEntity.name,
+            latitude = placeJpaEntity.latitude,
+            longitude = placeJpaEntity.longitude,
+            address = placeJpaEntity.address,
+            kakaoMapPlaceId = placeJpaEntity.kakaoMapPlaceId,
+            averageRating = placeJpaEntity.averageRating,
+            reviewCount = placeJpaEntity.reviewCount,
+        )
+
+    fun toEntity(place: Place): PlaceJpaEntity =
+        PlaceJpaEntity(
+            id = place.id,
+            name = place.name,
+            latitude = place.latitude,
+            longitude = place.longitude,
+            address = place.address,
+            kakaoMapPlaceId = place.kakaoMapPlaceId,
+            averageRating = place.averageRating,
+            reviewCount = place.reviewCount,
+        )
+}
