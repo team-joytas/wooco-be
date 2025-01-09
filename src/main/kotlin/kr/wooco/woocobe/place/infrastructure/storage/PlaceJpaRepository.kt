084172fde5ce0bf15c17724c38da21e646ceea85
Namgyu11 <103015031+Namgyu11@users.noreply.github.com>
2025-01-09 14:49:52 +0900

    refactor: JPA 리포지토리 경로 정리 및 리팩토링  (#70)

--- src/main/kotlin/kr/wooco/woocobe/place/infrastructure/storage/PlaceJpaRepository.kt
+++
@@ -1,9 +0,0 @@
-package kr.wooco.woocobe.place.infrastructure.storage
-
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface PlaceJpaRepository : JpaRepository<PlaceEntity, Long> {
-    fun findByKakaoMapPlaceId(kakaoMapPlaceId: String): PlaceEntity?
-
-    fun findAllByIdIn(placeIds: List<Long>): List<PlaceEntity>
-}
