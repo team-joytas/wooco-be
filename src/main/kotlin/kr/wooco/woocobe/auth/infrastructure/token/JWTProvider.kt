330323e443a43784fd945281423cbb44d1115eb8
JiHongKim98 <kinjihong9598@gmail.com>
2025-01-09 16:13:25 +0900

    fix: 식별자 직렬화시 오차가 발생하는 오류 수정 (#74)

--- src/main/kotlin/kr/wooco/woocobe/auth/infrastructure/token/JWTProvider.kt
+++ src/main/kotlin/kr/wooco/woocobe/auth/infrastructure/token/JWTProvider.kt
@@ -1,7 +1,5 @@
 package kr.wooco.woocobe.auth.infrastructure.token
 
-import io.jsonwebtoken.Claims
-import io.jsonwebtoken.ExpiredJwtException
 import io.jsonwebtoken.JwtParser
 import io.jsonwebtoken.Jwts
 import io.jsonwebtoken.security.Keys
@@ -16,11 +14,8 @@ class JWTProvider(
     @Value("\${app.jwt.expiration.access-token}") private val accessTokenExpiration: Long,
     @Value("\${app.jwt.expiration.refresh-token}") private val refreshTokenExpiration: Long,
 ) {
-    private final val secretKey: Key =
-        Keys.hmacShaKeyFor(signingKey.toByteArray())
-
-    private val parser: JwtParser =
-        Jwts.parserBuilder().setSigningKey(secretKey).build()
+    private val secretKey: Key = Keys.hmacShaKeyFor(signingKey.toByteArray())
+    private val jwtParser: JwtParser = Jwts.parserBuilder().setSigningKey(secretKey).build()
 
     fun generateAccessToken(authUserId: Long): String = createToken(USER_ID, authUserId, accessTokenExpiration)
 
@@ -28,16 +23,24 @@ class JWTProvider(
 
     fun extractUserId(token: String): Long =
         runCatching {
-            parseClaimsBody(token)[USER_ID]!!.toString().toLong()
+            jwtParser
+                .parseClaimsJws(token)
+                .body[USER_ID]
+                .toString()
+                .toLong()
         }.getOrElse {
-            throw RuntimeException(it)
+            throw RuntimeException()
         }
 
     fun extractTokenId(token: String): Long =
         runCatching {
-            parseClaimsBody(token)[TOKEN_ID]!!.toString().toLong()
+            jwtParser
+                .parseClaimsJws(token)
+                .body[TOKEN_ID]
+                .toString()
+                .toLong()
         }.getOrElse {
-            throw RuntimeException(it)
+            throw RuntimeException()
         }
 
     private fun createToken(
@@ -47,20 +50,10 @@ class JWTProvider(
     ): String =
         Jwts
             .builder()
-            .addClaims(mapOf(key to value))
-            .setExpiration(Date(System.currentTimeMillis() + expiredAt))
             .signWith(secretKey)
+            .addClaims(mapOf(key to value.toString()))
+            .setExpiration(Date(System.currentTimeMillis().plus(expiredAt)))
             .compact()
-
-    private fun parseClaimsBody(token: String): Claims =
-        runCatching {
-            parser.parseClaimsJws(token).body
-        }.getOrElse { throwable ->
-            when (throwable) {
-                is ExpiredJwtException -> throw RuntimeException()
-                else -> throw RuntimeException()
-            }
-        }
 
     companion object {
         private const val USER_ID = "user_id"
