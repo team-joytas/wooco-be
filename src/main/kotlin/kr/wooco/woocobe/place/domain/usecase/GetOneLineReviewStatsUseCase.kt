cfe451fe9b3eb24cd30dc4780fc02648f5dc9b0b
Namgyu11 <103015031+Namgyu11@users.noreply.github.com>
2025-01-09 18:08:54 +0900

    fix: @Service 어노테이션 누락 수정 (#70)

--- src/main/kotlin/kr/wooco/woocobe/place/domain/usecase/GetOneLineReviewStatsUseCase.kt
+++ src/main/kotlin/kr/wooco/woocobe/place/domain/usecase/GetOneLineReviewStatsUseCase.kt
@@ -3,6 +3,7 @@ package kr.wooco.woocobe.place.domain.usecase
 import kr.wooco.woocobe.common.domain.usecase.UseCase
 import kr.wooco.woocobe.place.domain.gateway.PlaceStorageGateway
 import kr.wooco.woocobe.place.domain.model.PlaceOneLineReviewStat
+import org.springframework.stereotype.Service
 
 data class GetOneLineReviewStatsInput(
     val placeId: Long,
@@ -12,6 +13,7 @@ data class GetOneLineReviewStatsOutput(
     val placeOneLineReviewRank: List<PlaceOneLineReviewStat>,
 )
 
+@Service
 class GetOneLineReviewStatsUseCase(
     private val placeStorageGateway: PlaceStorageGateway,
 ) : UseCase<GetOneLineReviewStatsInput, GetOneLineReviewStatsOutput> {
