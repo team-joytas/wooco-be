95e6d6b29cf8ee10b516a7fb098749c5380dae9b
JiHongKim98 <kinjihong9598@gmail.com>
2025-01-10 14:57:05 +0900

    refactor: 회원 엔티티 매퍼 클래스 추가 (#64)

--- src/main/kotlin/kr/wooco/woocobe/user/infrastructure/gateway/JpaUserStorageGateway.kt
+++ src/main/kotlin/kr/wooco/woocobe/user/infrastructure/gateway/JpaUserStorageGateway.kt
@@ -2,7 +2,7 @@ package kr.wooco.woocobe.user.infrastructure.gateway
 
 import kr.wooco.woocobe.user.domain.gateway.UserStorageGateway
 import kr.wooco.woocobe.user.domain.model.User
-import kr.wooco.woocobe.user.infrastructure.storage.entity.UserEntity
+import kr.wooco.woocobe.user.infrastructure.storage.UserStorageMapper
 import kr.wooco.woocobe.user.infrastructure.storage.repository.UserJpaRepository
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Component
@@ -10,12 +10,21 @@ import org.springframework.stereotype.Component
 @Component
 internal class JpaUserStorageGateway(
     private val userJpaRepository: UserJpaRepository,
+    private val userStorageMapper: UserStorageMapper,
 ) : UserStorageGateway {
-    override fun save(user: User): User = userJpaRepository.save(UserEntity.from(user)).toDomain()
+    override fun save(user: User): User {
+        val userEntity = userStorageMapper.toEntity(user)
+        userJpaRepository.save(userEntity)
+        return userStorageMapper.toDomain(userEntity)
+    }
 
-    override fun update(user: User) = userJpaRepository.save(UserEntity.fromWithId(user)).toDomain()
+    override fun getByUserId(userId: Long): User? {
+        val userEntity = userJpaRepository.findByIdOrNull(userId)
+            ?: throw RuntimeException()
+        return userStorageMapper.toDomain(userEntity)
+    }
 
-    override fun getByUserId(userId: Long): User? = userJpaRepository.findByIdOrNull(userId)?.toDomain()
-
-    override fun deleteByUserId(userId: Long) = userJpaRepository.deleteById(userId)
+    override fun deleteByUserId(userId: Long) {
+        userJpaRepository.deleteById(userId)
+    }
 }
---
+++ src/main/kotlin/kr/wooco/woocobe/user/infrastructure/storage/UserStorageMapper.kt
@@ -0,0 +1,22 @@
+package kr.wooco.woocobe.user.infrastructure.storage
+
+import kr.wooco.woocobe.user.domain.model.User
+import kr.wooco.woocobe.user.infrastructure.storage.entity.UserJpaEntity
+import org.springframework.stereotype.Component
+
+@Component
+class UserStorageMapper {
+    fun toDomain(userJpaEntity: UserJpaEntity): User =
+        User(
+            id = userJpaEntity.id,
+            name = userJpaEntity.name,
+            profileUrl = userJpaEntity.profileUrl,
+        )
+
+    fun toEntity(user: User): UserJpaEntity =
+        UserJpaEntity(
+            id = user.id,
+            name = user.name,
+            profileUrl = user.profileUrl,
+        )
+}
