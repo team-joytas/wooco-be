a3ba25a5ace3dc07a5e12acab956b19b155f00d8
Namgyu11 <103015031+Namgyu11@users.noreply.github.com>
2025-01-11 14:02:41 +0900

    feat: GetAllPlaceUseCase에 @Service 어노테이션 추가 (#82)

--- src/main/kotlin/kr/wooco/woocobe/place/domain/usecase/GetAllPlaceUseCase.kt
+++ src/main/kotlin/kr/wooco/woocobe/place/domain/usecase/GetAllPlaceUseCase.kt
@@ -3,6 +3,7 @@ package kr.wooco.woocobe.place.domain.usecase
 import kr.wooco.woocobe.common.domain.usecase.UseCase
 import kr.wooco.woocobe.place.domain.gateway.PlaceStorageGateway
 import kr.wooco.woocobe.place.domain.model.Place
+import org.springframework.stereotype.Service
 
 data class GetAllPlaceInput(
     val placeIds: List<Long>,
@@ -12,6 +13,7 @@ data class GetAllPlaceOutput(
     val places: List<Place>,
 )
 
+@Service
 class GetAllPlaceUseCase(
     private val placeStorageGateway: PlaceStorageGateway,
 ) : UseCase<GetAllPlaceInput, GetAllPlaceOutput> {
