9376799ae5a90d301497245ff45d8dcde6e463aa
Namgyu11 <103015031+Namgyu11@users.noreply.github.com>
2025-01-11 13:01:18 +0900

    style: GetAllPlaceUseCase 관련 데이터 클래스 및 함수명 수정 (#82)

--- src/main/kotlin/kr/wooco/woocobe/place/domain/usecase/GetAllPlaceUseCase.kt
+++ src/main/kotlin/kr/wooco/woocobe/place/domain/usecase/GetAllPlaceUseCase.kt
@@ -4,21 +4,21 @@ import kr.wooco.woocobe.common.domain.usecase.UseCase
 import kr.wooco.woocobe.place.domain.gateway.PlaceStorageGateway
 import kr.wooco.woocobe.place.domain.model.Place
 
-data class GetAllPlaceCaseInput(
+data class GetAllPlaceInput(
     val placeIds: List<Long>,
 )
 
-data class GetAllPlaceCaseOutput(
+data class GetAllPlaceOutput(
     val places: List<Place>,
 )
 
 class GetAllPlaceUseCase(
     private val placeStorageGateway: PlaceStorageGateway,
-) : UseCase<GetAllPlaceCaseInput, GetAllPlaceCaseOutput> {
-    override fun execute(input: GetAllPlaceCaseInput): GetAllPlaceCaseOutput {
+) : UseCase<GetAllPlaceInput, GetAllPlaceOutput> {
+    override fun execute(input: GetAllPlaceInput): GetAllPlaceOutput {
         val places = placeStorageGateway.getAllByPlaceIds(input.placeIds)
 
-        return GetAllPlaceCaseOutput(
+        return GetAllPlaceOutput(
             places = places,
         )
     }
