6f76ad83c3b621469977cb77f9a44931ccf1e4a6
JiHongKim98 <kinjihong9598@gmail.com>
2025-01-13 20:09:21 +0900

    refactor: 코스 조회시 코스 도메인만 반환하도록 수정 (#93)

--- src/main/kotlin/kr/wooco/woocobe/course/domain/usecase/GetCourseUseCase.kt
+++ src/main/kotlin/kr/wooco/woocobe/course/domain/usecase/GetCourseUseCase.kt
@@ -2,39 +2,30 @@ package kr.wooco.woocobe.course.domain.usecase
 
 import kr.wooco.woocobe.common.domain.usecase.UseCase
 import kr.wooco.woocobe.course.domain.gateway.CourseStorageGateway
-import kr.wooco.woocobe.course.domain.gateway.InterestCourseStorageGateway
 import kr.wooco.woocobe.course.domain.model.Course
 import org.springframework.stereotype.Service
 
 data class GetCourseInput(
-    val userId: Long?,
     val courseId: Long,
 )
 
 data class GetCourseOutput(
     val course: Course,
-    val isInterested: Boolean,
 )
 
+// TODO 이벤트 기반 고려 :: Query 작업에 Command 작업이 껴있다.
 @Service
 class GetCourseUseCase(
     private val courseStorageGateway: CourseStorageGateway,
-    private val interestCourseStorageGateway: InterestCourseStorageGateway,
 ) : UseCase<GetCourseInput, GetCourseOutput> {
     override fun execute(input: GetCourseInput): GetCourseOutput {
         val course = courseStorageGateway.getByCourseId(input.courseId)
 
-        val isInterested = input.userId?.run {
-            interestCourseStorageGateway.existsByCourseIdAndUserId(input.courseId, input.userId)
-        } ?: false
-
-        // TODO 이벤트 기반 고려 :: Query 작업에 Command 작업이 껴있다.
         course.increaseViews()
         courseStorageGateway.save(course)
 
         return GetCourseOutput(
             course = course,
-            isInterested = isInterested,
         )
     }
 }
