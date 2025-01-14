eb522e9d63f4615c0c7a325e03441eafd118872a
jspark <98972385+JunseoParKK@users.noreply.github.com>
2025-01-14 14:07:45 +0900

    refactor: 플랜 수정 요청 구조 변경 (#81)

--- src/main/kotlin/kr/wooco/woocobe/plan/ui/web/controller/request/UpdatePlanRequest.kt
+++ src/main/kotlin/kr/wooco/woocobe/plan/ui/web/controller/request/UpdatePlanRequest.kt
@@ -1,5 +1,6 @@
 package kr.wooco.woocobe.plan.ui.web.controller.request
 
+import kr.wooco.woocobe.plan.domain.usecase.UpdatePlanInput
 import java.time.LocalDate
 
 data class UpdatePlanRequest(
@@ -10,4 +11,20 @@ data class UpdatePlanRequest(
     val visitDate: LocalDate,
     val placeIds: List<Long>,
     val categories: List<String>,
-)
+) {
+    fun toCommand(
+        userId: Long,
+        planId: Long,
+    ): UpdatePlanInput =
+        UpdatePlanInput(
+            userId = userId,
+            planId = planId,
+            title = title,
+            description = description,
+            primaryRegion = primaryRegion,
+            secondaryRegion = secondaryRegion,
+            visitDate = visitDate,
+            placeIds = placeIds,
+            categories = categories,
+        )
+}
