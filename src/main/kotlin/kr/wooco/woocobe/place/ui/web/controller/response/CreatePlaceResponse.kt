18f4b1904c3899a76ffb3d2ee03c15df0ee25fcf
Namgyu11 <103015031+Namgyu11@users.noreply.github.com>
2025-01-15 22:29:48 +0900

    refactor: CreatePlaceResponse의 불필요한 companion object 제거 (#94)

--- src/main/kotlin/kr/wooco/woocobe/place/ui/web/controller/response/CreatePlaceResponse.kt
+++ src/main/kotlin/kr/wooco/woocobe/place/ui/web/controller/response/CreatePlaceResponse.kt
@@ -2,11 +2,4 @@ package kr.wooco.woocobe.place.ui.web.controller.response
 
 data class CreatePlaceResponse(
     val placeId: Long,
-) {
-    companion object {
-        fun of(placeId: Long): CreatePlaceResponse =
-            CreatePlaceResponse(
-                placeId = placeId,
-            )
-    }
-}
+)
