96462cdd3f7fc8afcba17c876bbfa2727601ebce
Namgyu11 <103015031+Namgyu11@users.noreply.github.com>
2025-01-16 02:45:55 +0900

    feat: CreatePlaceRequest에 toCommand 변환 메서드 추가 (#94)

--- src/main/kotlin/kr/wooco/woocobe/place/ui/web/controller/request/CreatePlaceRequest.kt
+++ src/main/kotlin/kr/wooco/woocobe/place/ui/web/controller/request/CreatePlaceRequest.kt
@@ -1,9 +1,21 @@
 package kr.wooco.woocobe.place.ui.web.controller.request
 
+import kr.wooco.woocobe.place.domain.usecase.AddPlaceUseCaseInput
+
 data class CreatePlaceRequest(
     val name: String,
     val latitude: Double,
     val longitude: Double,
     val address: String,
     val kakaoMapPlaceId: String,
-)
+) {
+    fun toCommand(userId: Long): AddPlaceUseCaseInput =
+        AddPlaceUseCaseInput(
+            userId = userId,
+            name = name,
+            latitude = latitude,
+            longitude = longitude,
+            address = address,
+            kakaoMapPlaceId = kakaoMapPlaceId,
+        )
+}
