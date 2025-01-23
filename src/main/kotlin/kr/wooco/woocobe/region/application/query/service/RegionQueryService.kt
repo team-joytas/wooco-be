32cd608152568bba155625e8f850f573366d971f
JiHongKim98 <kinjihong9598@gmail.com>
2025-01-23 10:48:16 +0900

    feat: 선호 지역 조회 로직 추가 (#123)

---
+++ src/main/kotlin/kr/wooco/woocobe/region/application/query/service/RegionQueryService.kt
@@ -0,0 +1,28 @@
+package kr.wooco.woocobe.region.application.query.service
+
+import kr.wooco.woocobe.region.application.query.port.`in`.GetAllPreferenceRegionQuery
+import kr.wooco.woocobe.region.application.query.port.`in`.GetAllPreferenceRegionUseCase
+import kr.wooco.woocobe.region.application.query.port.`in`.GetPreferenceRegionQuery
+import kr.wooco.woocobe.region.application.query.port.`in`.GetPreferenceRegionUseCase
+import kr.wooco.woocobe.region.application.query.port.out.LoadPreferenceRegionDataPort
+import kr.wooco.woocobe.region.application.query.service.dto.PreferenceRegionData
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+internal class RegionQueryService(
+    private val loadPreferenceRegionDataPort: LoadPreferenceRegionDataPort,
+) : GetPreferenceRegionUseCase,
+    GetAllPreferenceRegionUseCase {
+    @Transactional(readOnly = true)
+    override fun getPreferenceRegion(query: GetPreferenceRegionQuery): PreferenceRegionData =
+        loadPreferenceRegionDataPort.getByUserIdAndRegion(
+            userId = query.userId,
+            primaryRegion = query.primaryRegion,
+            secondaryRegion = query.secondaryRegion,
+        )
+
+    @Transactional(readOnly = true)
+    override fun getAllPreferenceRegion(query: GetAllPreferenceRegionQuery): List<PreferenceRegionData> =
+        loadPreferenceRegionDataPort.getAllByUserId(query.userId)
+}
