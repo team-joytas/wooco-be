e399c124f0b6ffd8e019f910c888a1766e2c8e02
JiHongKim98 <kinjihong9598@gmail.com>
2025-02-04 17:01:21 +0900

    chore: 기존 유즈케이스 deprecated 어노테이션 추가 (#133)

--- src/main/kotlin/kr/wooco/woocobe/user/domain/usecase/GetAllUserUseCase.kt
+++ src/main/kotlin/kr/wooco/woocobe/user/domain/usecase/GetAllUserUseCase.kt
@@ -1,7 +1,9 @@
+// FIXME: 삭제
+
 package kr.wooco.woocobe.user.domain.usecase
 
 import kr.wooco.woocobe.common.domain.usecase.UseCase
-import kr.wooco.woocobe.user.domain.gateway.UserStorageGateway
+import kr.wooco.woocobe.user.application.port.out.LoadUserPersistencePort
 import kr.wooco.woocobe.user.domain.entity.User
 import org.springframework.stereotype.Service
 
@@ -15,10 +17,11 @@ data class GetAllUserOutput(
 
 @Service
 class GetAllUserUseCase(
-    private val userStorageGateway: UserStorageGateway,
+    private val loadUserPersistencePort: LoadUserPersistencePort,
 ) : UseCase<GetAllUserInput, GetAllUserOutput> {
+    @Deprecated(message = "outbound port 를 통해 필요한 데이터를 가져오는 방식으로 수정해주세요.")
     override fun execute(input: GetAllUserInput): GetAllUserOutput {
-        val users = userStorageGateway.getAllByUserIds(input.userIds)
+        val users = loadUserPersistencePort.getAllByUserIds(input.userIds)
 
         return GetAllUserOutput(
             users = users,
--- src/main/kotlin/kr/wooco/woocobe/user/domain/usecase/GetUserUseCase.kt
+++ src/main/kotlin/kr/wooco/woocobe/user/domain/usecase/GetUserUseCase.kt
@@ -1,7 +1,9 @@
+// FIXME: 삭제
+
 package kr.wooco.woocobe.user.domain.usecase
 
 import kr.wooco.woocobe.common.domain.usecase.UseCase
-import kr.wooco.woocobe.user.domain.gateway.UserStorageGateway
+import kr.wooco.woocobe.user.application.port.out.LoadUserPersistencePort
 import kr.wooco.woocobe.user.domain.entity.User
 import org.springframework.stereotype.Service
 
@@ -15,10 +17,11 @@ data class GetUserOutput(
 
 @Service
 class GetUserUseCase(
-    private val userStorageGateway: UserStorageGateway,
+    private val loadUserPersistencePort: LoadUserPersistencePort,
 ) : UseCase<GetUserInput, GetUserOutput> {
+    @Deprecated(message = "outbound port 를 통해 필요한 데이터를 가져오는 방식으로 수정해주세요.")
     override fun execute(input: GetUserInput): GetUserOutput {
-        val user = userStorageGateway.getByUserId(userId = input.userId)
+        val user = loadUserPersistencePort.getByUserId(input.userId)
 
         return GetUserOutput(
             user = user,
