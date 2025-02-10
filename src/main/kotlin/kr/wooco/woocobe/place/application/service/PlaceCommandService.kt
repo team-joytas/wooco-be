7e6805b65ca5b538407190933494459abf2512f1
Namgyu11 <103015031+Namgyu11@users.noreply.github.com>
2025-02-10 15:43:36 +0900

    refactor: createPlace 메서드 가독성 개선 및 변수 활용 추가 (#137)

--- src/main/kotlin/kr/wooco/woocobe/place/application/service/PlaceCommandService.kt
+++ src/main/kotlin/kr/wooco/woocobe/place/application/service/PlaceCommandService.kt
@@ -13,17 +13,18 @@ internal class PlaceCommandService(
     private val loadPlacePersistencePort: LoadPlacePersistencePort,
 ) : CreatePlaceUseCase {
     @Transactional
-    override fun createPlace(command: CreatePlaceUseCase.Command): Long =
-        loadPlacePersistencePort.getOrNullByKakaoMapPlaceId(command.kakaoPlaceId)?.id
-            ?: savePlacePersistencePort
-                .savePlace(
-                    Place.create(
-                        name = command.name,
-                        kakaoPlaceId = command.kakaoPlaceId,
-                        address = command.address,
-                        latitude = command.latitude,
-                        longitude = command.longitude,
-                        phoneNumber = command.phoneNumber,
-                    ),
-                ).id
+    override fun createPlace(command: CreatePlaceUseCase.Command): Long {
+        val place = loadPlacePersistencePort.getOrNullByKakaoMapPlaceId(command.kakaoPlaceId)
+            ?: savePlacePersistencePort.savePlace(
+                Place.create(
+                    name = command.name,
+                    kakaoPlaceId = command.kakaoPlaceId,
+                    address = command.address,
+                    latitude = command.latitude,
+                    longitude = command.longitude,
+                    phoneNumber = command.phoneNumber,
+                ),
+            )
+        return place.id
+    }
 }
