12d7f3401be4c81ee368b39a02dbeb26fcba8367
Namgyu11 <103015031+Namgyu11@users.noreply.github.com>
2025-02-20 03:29:09 +0900

    refactor: PlaceOneLineReviewStat 관련 코드 삭제 (#158)

--- core/src/main/kotlin/kr/wooco/woocobe/core/placereview/application/port/out/LoadPlaceOneLineReviewStatPersistencePort.kt
+++
@@ -1,9 +0,0 @@
-package kr.wooco.woocobe.core.placereview.application.port.out
-
-import kr.wooco.woocobe.core.placereview.domain.entity.PlaceOneLineReviewStat
-
-interface LoadPlaceOneLineReviewStatPersistencePort {
-    fun getAllStatsByPlaceId(placeId: Long): List<PlaceOneLineReviewStat>
-
-    fun getAllStatsByPlaceIds(placeIds: List<Long>): List<PlaceOneLineReviewStat>
-}
