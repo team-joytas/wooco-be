210cd3847529bc4548464e14490e2187cefee8b4
jspark <98972385+JunseoParKK@users.noreply.github.com>
2025-02-22 03:33:58 +0900

    feat: FCM 메시지 전송을 위한 어댑터 및 포트 구현 (#16)

---
+++ infrastructure/fcm/src/main/kotlin/kr/wooco/woocobe/fcm/notification/SendNotificationFcmAdapter.kt
@@ -0,0 +1,27 @@
+package kr.wooco.woocobe.fcm.notification
+
+import com.google.firebase.messaging.FirebaseMessaging
+import com.google.firebase.messaging.Message
+import kr.wooco.woocobe.core.notification.application.port.out.SendNotificationPort
+import kr.wooco.woocobe.core.notification.domain.entity.Notification
+import org.springframework.stereotype.Component
+
+@Component
+internal class SendNotificationFcmAdapter : SendNotificationPort {
+    override fun sendNotification(
+        notification: Notification,
+        token: String,
+    ) {
+        val message = Message
+            .builder()
+            .setToken(token)
+            .putData("notificationId", notification.id.toString())
+            .putData("userId", notification.userId.toString())
+            .putData("targetId", notification.targetId.toString())
+            .putData("targetName", notification.targetName)
+            .putData("type", notification.type.name)
+            .putData("isRead", notification.isRead.toString())
+            .build()
+        FirebaseMessaging.getInstance().send(message)
+    }
+}
