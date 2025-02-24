18e0448bf410893957b3c6ce2dec0756e7790b92
JiHongKim98 <kinjihong9598@gmail.com>
2025-02-24 17:46:48 +0900

    refactor: 비동기 쓰레드 풀 설정 수정 (#165)

--- bootstrap/api/src/main/kotlin/kr/wooco/woocobe/api/common/config/AsyncConfig.kt
+++ bootstrap/api/src/main/kotlin/kr/wooco/woocobe/api/common/config/AsyncConfig.kt
@@ -7,25 +7,31 @@ import org.springframework.core.task.TaskDecorator
 import org.springframework.scheduling.annotation.AsyncConfigurer
 import org.springframework.scheduling.annotation.EnableAsync
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor
+import java.time.Duration
 import java.util.concurrent.Executor
 
 @EnableAsync
 @Configuration
 class AsyncConfig(
-    @Value("\${spring.threads.async.pool.max}") private val asyncThreadMaxPoolSize: Int,
-    @Value("\${spring.threads.async.pool.core}") private val asyncThreadCorePoolSize: Int,
+    @Value("\${spring.async-task-pool.max-size}") private val asyncThreadMaxPoolSize: Int,
+    @Value("\${spring.async-task-pool.core-size}") private val asyncThreadCorePoolSize: Int,
+    @Value("\${spring.async-task-pool.queue-capacity}") private val asyncThreadQueueCapacity: Int,
+    @Value("\${spring.async-task-pool.await-duration}") private val asyncAwaitTerminationDuration: Duration,
 ) : AsyncConfigurer {
     override fun getAsyncExecutor(): Executor =
         ThreadPoolTaskExecutor().apply {
             maxPoolSize = asyncThreadMaxPoolSize
             corePoolSize = asyncThreadCorePoolSize
+            queueCapacity = asyncThreadQueueCapacity
             setThreadNamePrefix(ASYNC_THREAD_PREFIX)
+            setWaitForTasksToCompleteOnShutdown(true)
+            setAwaitTerminationSeconds(asyncAwaitTerminationDuration.toSeconds().toInt())
             setTaskDecorator(MdcDecorator)
             initialize()
         }
 
     companion object {
-        private const val ASYNC_THREAD_PREFIX = "async-thread-"
+        private const val ASYNC_THREAD_PREFIX = "async-task-"
 
         object MdcDecorator : TaskDecorator {
             override fun decorate(runnable: Runnable): Runnable =
