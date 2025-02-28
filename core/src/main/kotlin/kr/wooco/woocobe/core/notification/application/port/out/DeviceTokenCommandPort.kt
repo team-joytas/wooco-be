1f185d793d2291eaf4dbbbf4105121c2266e3224
jspark <98972385+JunseoParKK@users.noreply.github.com>
2025-03-01 02:42:19 +0900

    refactor: DeviceTokenCommandPort 통합 (#173)

--- core/src/main/kotlin/kr/wooco/woocobe/core/notification/application/port/out/DeviceTokenCommandPort.kt
+++ core/src/main/kotlin/kr/wooco/woocobe/core/notification/application/port/out/DeviceTokenCommandPort.kt
@@ -1,7 +1,10 @@
 package kr.wooco.woocobe.core.notification.application.port.out
 
 import kr.wooco.woocobe.core.notification.domain.entity.DeviceToken
+import kr.wooco.woocobe.core.notification.domain.vo.Token
 
-interface SaveDeviceTokenPersistencePort {
+interface DeviceTokenCommandPort {
     fun saveDeviceToken(deviceToken: DeviceToken): DeviceToken
+
+    fun deleteByToKen(token: Token)
 }
