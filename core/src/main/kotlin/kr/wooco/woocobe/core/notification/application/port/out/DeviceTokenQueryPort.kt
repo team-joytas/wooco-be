107e08d2fa3b5cbc8d41c16a88bf9731194444ff
jspark <98972385+JunseoParKK@users.noreply.github.com>
2025-03-01 02:42:56 +0900

    refactor: DeviceTokenQueryPort 통합 (#173)

---
+++ core/src/main/kotlin/kr/wooco/woocobe/core/notification/application/port/out/DeviceTokenQueryPort.kt
@@ -0,0 +1,10 @@
+package kr.wooco.woocobe.core.notification.application.port.out
+
+import kr.wooco.woocobe.core.notification.domain.entity.DeviceToken
+import kr.wooco.woocobe.core.notification.domain.vo.Token
+
+interface DeviceTokenQueryPort {
+    fun getByToken(token: Token): DeviceToken
+
+    fun getAllByUserId(userId: Long): List<DeviceToken>
+}
