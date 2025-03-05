6e8af01f720906f513d36d907d21d769fb2d1cd4
jspark <98972385+JunseoParKK@users.noreply.github.com>
2025-03-06 02:58:31 +0900

    refactor: 알림 매퍼 object 로 변경 (#173)

--- infrastructure/mysql/src/main/kotlin/kr/wooco/woocobe/mysql/notification/NotificationPersistenceMapper.kt
+++ infrastructure/mysql/src/main/kotlin/kr/wooco/woocobe/mysql/notification/NotificationPersistenceMapper.kt
@@ -1,30 +1,29 @@
 package kr.wooco.woocobe.mysql.notification
 
 import kr.wooco.woocobe.core.notification.domain.entity.Notification
+import kr.wooco.woocobe.core.notification.domain.vo.NotificationReadStatus
 import kr.wooco.woocobe.core.notification.domain.vo.NotificationType
 import kr.wooco.woocobe.mysql.notification.entity.NotificationJpaEntity
-import org.springframework.stereotype.Component
 
-@Component
-internal class NotificationPersistenceMapper {
-    fun toDomain(notificationJpaEntity: NotificationJpaEntity): Notification =
+internal object NotificationPersistenceMapper {
+    fun toDomainEntity(notificationJpaEntity: NotificationJpaEntity): Notification =
         Notification(
             id = notificationJpaEntity.id,
             userId = notificationJpaEntity.userId,
             targetId = notificationJpaEntity.targetId,
             targetName = notificationJpaEntity.targetName,
-            isRead = notificationJpaEntity.isRead,
             type = NotificationType.invoke(notificationJpaEntity.type),
-            sentAt = notificationJpaEntity.createdAt,
+            createdAt = notificationJpaEntity.createdAt,
+            readStatus = NotificationReadStatus.invoke(notificationJpaEntity.readStatus),
         )
 
-    fun toEntity(notification: Notification): NotificationJpaEntity =
+    fun toJpaEntity(notification: Notification): NotificationJpaEntity =
         NotificationJpaEntity(
             id = notification.id,
             userId = notification.userId,
             targetId = notification.targetId,
             targetName = notification.targetName,
-            isRead = notification.isRead,
             type = notification.type.name,
+            readStatus = notification.readStatus.name,
         )
 }
