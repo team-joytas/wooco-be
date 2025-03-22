f1d0953989885002e80ca22e5327157ac24f0d9c
jspark <98972385+JunseoParKK@users.noreply.github.com>
2025-03-23 00:37:00 +0900

    refactor: 디바이스 토큰 매퍼 및 어댑터 네이밍 변경 (#202)

--- infrastructure/mysql/src/main/kotlin/kr/wooco/woocobe/mysql/notification/DeviceTokenJpaMapper.kt
+++ infrastructure/mysql/src/main/kotlin/kr/wooco/woocobe/mysql/notification/DeviceTokenJpaMapper.kt
@@ -1,15 +1,17 @@
 package kr.wooco.woocobe.mysql.notification
 
 import kr.wooco.woocobe.core.notification.domain.entity.DeviceToken
+import kr.wooco.woocobe.core.notification.domain.vo.DeviceTokenStatus
 import kr.wooco.woocobe.core.notification.domain.vo.Token
 import kr.wooco.woocobe.mysql.notification.entity.DeviceTokenJpaEntity
 
-internal object DeviceTokenPersistenceMapper {
+internal object DeviceTokenJpaMapper {
     fun toDomainEntity(deviceTokenJpaEntity: DeviceTokenJpaEntity): DeviceToken =
         DeviceToken(
             id = deviceTokenJpaEntity.id,
             userId = deviceTokenJpaEntity.userId,
             token = Token(deviceTokenJpaEntity.token),
+            status = DeviceTokenStatus(deviceTokenJpaEntity.status),
         )
 
     fun toJpaEntity(deviceToken: DeviceToken): DeviceTokenJpaEntity =
@@ -17,5 +19,6 @@ internal object DeviceTokenPersistenceMapper {
             id = deviceToken.id,
             userId = deviceToken.userId,
             token = deviceToken.token.value,
+            status = deviceToken.status.name,
         )
 }
