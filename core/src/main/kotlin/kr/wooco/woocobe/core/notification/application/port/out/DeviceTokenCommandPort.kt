f96b82e9144b9086922895fc540c5188dcb4109a
jspark <98972385+JunseoParKK@users.noreply.github.com>
2025-03-29 00:51:23 +0900

    refactor: DeviceTokenCommandPort 에서 디바이스 토큰 삭제 메서드 제거 (#202)

--- core/src/main/kotlin/kr/wooco/woocobe/core/notification/application/port/out/DeviceTokenCommandPort.kt
+++ core/src/main/kotlin/kr/wooco/woocobe/core/notification/application/port/out/DeviceTokenCommandPort.kt
@@ -1,10 +1,7 @@
 package kr.wooco.woocobe.core.notification.application.port.out
 
 import kr.wooco.woocobe.core.notification.domain.entity.DeviceToken
-import kr.wooco.woocobe.core.notification.domain.vo.Token
 
 interface DeviceTokenCommandPort {
     fun saveDeviceToken(deviceToken: DeviceToken): DeviceToken
-
-    fun deleteByToken(token: Token)
 }
