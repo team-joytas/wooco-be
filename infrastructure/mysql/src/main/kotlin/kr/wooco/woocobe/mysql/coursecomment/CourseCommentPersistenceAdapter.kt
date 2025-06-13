f614cc984d894dc4d39ba92c20972ddb6fd45963
JiHongKim98 <kinjihong9598@gmail.com>
2025-06-14 00:36:20 +0900

    refactor: 댓글 영속화 로직 리팩토링 (#215)

--- infrastructure/mysql/src/main/kotlin/kr/wooco/woocobe/mysql/coursecomment/CourseCommentPersistenceAdapter.kt
+++ infrastructure/mysql/src/main/kotlin/kr/wooco/woocobe/mysql/coursecomment/CourseCommentPersistenceAdapter.kt
@@ -1,39 +1,36 @@
 package kr.wooco.woocobe.mysql.coursecomment
 
-import kr.wooco.woocobe.core.coursecomment.application.port.out.DeleteCourseCommentPersistencePort
-import kr.wooco.woocobe.core.coursecomment.application.port.out.LoadCourseCommentPersistencePort
-import kr.wooco.woocobe.core.coursecomment.application.port.out.SaveCourseCommentPersistencePort
+import kr.wooco.woocobe.core.coursecomment.application.port.out.CourseCommentCommandPort
+import kr.wooco.woocobe.core.coursecomment.application.port.out.CourseCommentQueryPort
+import kr.wooco.woocobe.core.coursecomment.application.port.out.dto.CourseCommentView
 import kr.wooco.woocobe.core.coursecomment.domain.entity.CourseComment
 import kr.wooco.woocobe.core.coursecomment.domain.exception.NotExistsCommentException
 import kr.wooco.woocobe.mysql.coursecomment.repository.CourseCommentJpaRepository
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Transactional
 
 @Component
 internal class CourseCommentPersistenceAdapter(
     private val courseCommentJpaRepository: CourseCommentJpaRepository,
     private val courseCommentPersistenceMapper: CourseCommentPersistenceMapper,
-) : LoadCourseCommentPersistencePort,
-    SaveCourseCommentPersistencePort,
-    DeleteCourseCommentPersistencePort {
+) : CourseCommentQueryPort,
+    CourseCommentCommandPort {
+    override fun getAllViewByCourseId(courseId: Long): List<CourseCommentView> {
+        val courseCommentJpaEntities = courseCommentJpaRepository.findAllByCourseId(courseId)
+        return courseCommentJpaEntities.map { courseCommentPersistenceMapper.toReadModel(it) }
+    }
+
     override fun getByCourseCommentId(courseCommentId: Long): CourseComment {
         val courseCommentJpaEntity = courseCommentJpaRepository.findByIdOrNull(courseCommentId)
             ?: throw NotExistsCommentException
         return courseCommentPersistenceMapper.toDomain(courseCommentJpaEntity)
     }
 
-    override fun getAllByCourseId(courseId: Long): List<CourseComment> {
-        val courseCommentJpaEntities = courseCommentJpaRepository.findAllByCourseId(courseId)
-        return courseCommentJpaEntities.map { courseCommentPersistenceMapper.toDomain(it) }
-    }
-
-    override fun saveCourseComment(courseComment: CourseComment): CourseComment {
-        val courseCommentJpaEntity = courseCommentPersistenceMapper.toEntity(courseComment)
-        courseCommentJpaRepository.save(courseCommentJpaEntity)
-        return courseCommentPersistenceMapper.toDomain(courseCommentJpaEntity)
-    }
-
-    override fun deleteByCourseCommentId(courseCommentId: Long) {
-        courseCommentJpaRepository.deleteById(courseCommentId)
+    @Transactional
+    override fun saveCourseComment(courseComment: CourseComment): Long {
+        val courseCommentJpaEntity =
+            courseCommentJpaRepository.save(courseCommentPersistenceMapper.toEntity(courseComment))
+        return courseCommentJpaEntity.id
     }
 }
