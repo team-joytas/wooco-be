f5005c06327b2731ac52d14f2ad7c5881b011c5f
jspark <98972385+JunseoParKK@users.noreply.github.com>
2025-07-13 02:23:12 +0900

    refactor: 알림 인 포트 커멘드 컨벤션 적용 (#236)

--- core/src/main/kotlin/kr/wooco/woocobe/core/notification/application/port/in/CreateNotificationUseCase.kt
+++ core/src/main/kotlin/kr/wooco/woocobe/core/notification/application/port/in/CreateNotificationUseCase.kt
@@ -1,12 +1,26 @@
 package kr.wooco.woocobe.core.notification.application.port.`in`
 
+import kr.wooco.woocobe.core.notification.domain.command.CreateNotificationCommand
+import kr.wooco.woocobe.core.notification.domain.vo.NotificationTarget
+import kr.wooco.woocobe.core.notification.domain.vo.NotificationType
+
 fun interface CreateNotificationUseCase {
     data class Command(
         val userId: Long,
         val targetId: Long,
         val targetName: String,
         val type: String,
-    )
+    ) {
+        fun toCreateCommand(): CreateNotificationCommand =
+            CreateNotificationCommand(
+                userId = userId,
+                target = NotificationTarget(
+                    targetId = targetId,
+                    targetName = targetName,
+                    type = NotificationType.invoke(type),
+                ),
+            )
+    }
 
     fun createNotification(command: Command): Long
 }
--- core/src/main/kotlin/kr/wooco/woocobe/core/notification/application/port/in/DeleteDeviceTokenUseCase.kt
+++ core/src/main/kotlin/kr/wooco/woocobe/core/notification/application/port/in/DeleteDeviceTokenUseCase.kt
@@ -1,10 +1,17 @@
 package kr.wooco.woocobe.core.notification.application.port.`in`
 
+import kr.wooco.woocobe.core.notification.domain.command.DeleteDeviceTokenCommand
+
 fun interface DeleteDeviceTokenUseCase {
     data class Command(
         val userId: Long,
         val token: String,
-    )
+    ) {
+        fun toDeleteCommand(): DeleteDeviceTokenCommand =
+            DeleteDeviceTokenCommand(
+                userId = userId,
+            )
+    }
 
     fun deleteDeviceToken(command: Command)
 }
