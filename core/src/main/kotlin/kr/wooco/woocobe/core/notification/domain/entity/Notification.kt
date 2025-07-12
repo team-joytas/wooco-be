1519f396a4b0bef38163b8cd9d772b6838d14b44
jspark <98972385+JunseoParKK@users.noreply.github.com>
2025-07-13 03:39:44 +0900

    refactor: 알림 애그리거트 메서드 인자값 타입 변경 및 리팩토링 (#236)

--- core/src/main/kotlin/kr/wooco/woocobe/core/notification/domain/entity/Notification.kt
+++ core/src/main/kotlin/kr/wooco/woocobe/core/notification/domain/entity/Notification.kt
@@ -1,6 +1,9 @@
 package kr.wooco.woocobe.core.notification.domain.entity
 
 import kr.wooco.woocobe.core.common.domain.entity.AggregateRoot
+import kr.wooco.woocobe.core.notification.domain.command.CreateNotificationCommand
+import kr.wooco.woocobe.core.notification.domain.command.DeleteNotificationCommand
+import kr.wooco.woocobe.core.notification.domain.command.MarkAsReadNotificationCommand
 import kr.wooco.woocobe.core.notification.domain.exception.AlreadyDeletedNotificationException
 import kr.wooco.woocobe.core.notification.domain.exception.InvalidNotificationOwnerException
 import kr.wooco.woocobe.core.notification.domain.exception.NotExistsNotificationException
@@ -9,42 +12,53 @@ import kr.wooco.woocobe.core.notification.domain.vo.NotificationStatus
 import kr.wooco.woocobe.core.notification.domain.vo.NotificationTarget
 import java.time.LocalDateTime
 
+// TODO: View Model 분리
+
 data class Notification(
     override val id: Long,
     val userId: Long,
     val target: NotificationTarget,
-    val createdAt: LocalDateTime, // TODO: View Model 분리
+    val createdAt: LocalDateTime,
     val status: NotificationStatus,
     val readStatus: NotificationReadStatus,
 ) : AggregateRoot() {
-    fun markAsRead(userId: Long): Notification {
+    fun markAsRead(command: MarkAsReadNotificationCommand): Notification {
         when {
-            this.userId != userId -> throw InvalidNotificationOwnerException
+            this.userId != command.userId -> throw InvalidNotificationOwnerException
             this.status != NotificationStatus.ACTIVE -> throw NotExistsNotificationException
         }
         return copy(readStatus = NotificationReadStatus.READ)
     }
 
-    fun delete(userId: Long): Notification {
+    fun delete(command: DeleteNotificationCommand): Notification {
         when {
-            this.userId != userId -> throw InvalidNotificationOwnerException
+            this.userId != command.userId -> throw InvalidNotificationOwnerException
             this.status != NotificationStatus.ACTIVE -> throw AlreadyDeletedNotificationException
         }
         return copy(status = NotificationStatus.DELETED)
     }
 
     companion object {
+        /**
+         * 새로운 알림을 생성합니다.
+         *
+         * @param command Notification 엔티티 생성을 위한 Command 객체
+         * @param identifier 생성된 Notification 엔티티의 새로운 ID를 할당하는 메서드
+         * @author Junseoparkk
+         */
         fun create(
-            userId: Long,
-            notificationTarget: NotificationTarget,
+            command: CreateNotificationCommand,
+            identifier: (Notification) -> Long,
         ): Notification =
             Notification(
                 id = 0L,
-                userId = userId,
-                target = notificationTarget,
+                userId = command.userId,
+                target = command.target,
                 createdAt = LocalDateTime.now(),
                 status = NotificationStatus.ACTIVE,
                 readStatus = NotificationReadStatus.UNREAD,
-            )
+            ).let {
+                it.copy(id = identifier.invoke(it))
+            }
     }
 }
