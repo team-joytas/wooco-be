c7c4f0980561ea621f53650f52c6f61b38b7ecf9
박준서 <wnstj444@naver.com>
2025-11-11 00:52:33 +0900

    feat: 그룹 유스케이스 추가 (#243)

---
+++ core/src/main/kotlin/kr/wooco/woocobe/core/group/application/port/in/CreateGroupUseCase.kt
@@ -0,0 +1,21 @@
+package kr.wooco.woocobe.core.group.application.port.`in`
+
+import kr.wooco.woocobe.core.group.domain.command.CreateGroupCommand
+import kr.wooco.woocobe.core.group.domain.entity.Group
+
+fun interface CreateGroupUseCase {
+    data class Command(
+        val userId: Long,
+        val name: String,
+        val type: String,
+    ) {
+        fun toCreateCommand(): CreateGroupCommand =
+            CreateGroupCommand(
+                userId = userId,
+                name = Group.Name(name),
+                type = Group.Type.valueOf(type),
+            )
+    }
+
+    fun createGroup(command: CreateGroupUseCase.Command): Long
+}
