689ed09bd3ae60b672b66ecaef0ecd7807c5b671
박준서 <wnstj444@naver.com>
2025-12-21 17:06:23 +0900

    feat: 플랜 유스케이스 추가 (#255)

---
+++ core/src/main/kotlin/kr/wooco/woocobe/core/schedule/application/port/in/results/PlanResult.kt
@@ -0,0 +1,85 @@
+package kr.wooco.woocobe.core.schedule.application.port.`in`.results
+
+import kr.wooco.woocobe.core.group.application.port.`in`.results.GroupResult
+import kr.wooco.woocobe.core.place.domain.entity.Place
+import kr.wooco.woocobe.core.schedule.application.port.out.dto.PlanView
+import java.time.LocalDate
+
+data class PlanResult(
+    val id: Long,
+    val title: String,
+    val visitDate: LocalDate,
+    val group: GroupResult,
+    val places: List<PlanPlaceResult>,
+) {
+    data class PlanPlaceResult(
+        val order: Int,
+        val id: Long,
+        val name: String,
+        val latitude: Double,
+        val longitude: Double,
+        val address: String,
+        val thumbnailUrl: String,
+        val kakaoPlaceId: String,
+        val averageRating: Double,
+        val reviewCount: Long,
+    )
+
+    companion object {
+        fun of(
+            group: GroupResult,
+            planView: PlanView,
+            places: List<Place>,
+        ): PlanResult {
+            val placeMap = places.associateBy { it.id }
+            return of(group, planView, placeMap)
+        }
+
+        fun of(
+            group: GroupResult,
+            planView: PlanView,
+            placeMap: Map<Long, Place>,
+        ): PlanResult {
+            return PlanResult(
+                id = planView.id,
+                title = planView.title,
+                visitDate = planView.visitDate,
+                group = group,
+                places = toPlanPlaceResults(planView, placeMap),
+            )
+        }
+
+        fun listOf(
+            groups: Map<Long, GroupResult>,
+            planViews: List<PlanView>,
+            placeMap: Map<Long, Place>,
+        ): List<PlanResult> {
+            return planViews.map { planView ->
+                val group = requireNotNull(groups[planView.groupId])
+                of(group, planView, placeMap)
+            }
+        }
+
+        private fun toPlanPlaceResults(
+            planView: PlanView,
+            placeMap: Map<Long, Place>,
+        ): List<PlanPlaceResult> {
+            return planView.places.map { planPlace ->
+                val place = requireNotNull(placeMap[planPlace.placeId])
+
+                PlanPlaceResult(
+                    order = planPlace.order,
+                    id = place.id,
+                    name = place.name,
+                    latitude = place.latitude,
+                    longitude = place.longitude,
+                    address = place.address,
+                    thumbnailUrl = place.thumbnailUrl,
+                    kakaoPlaceId = place.kakaoPlaceId,
+                    averageRating = place.averageRating,
+                    reviewCount = place.reviewCount,
+                )
+            }
+        }
+    }
+}
