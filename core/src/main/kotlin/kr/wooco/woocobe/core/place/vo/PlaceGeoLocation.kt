c0990c3c744c2e35336f698da0d17556e4aee064
Namgyu11 <103015031+Namgyu11@users.noreply.github.com>
2026-01-09 21:36:02 +0900

    chore: 필요없는 클래스 삭제 (#261)

--- core/src/main/kotlin/kr/wooco/woocobe/core/place/vo/PlaceGeoLocation.kt
+++
@@ -1,6 +0,0 @@
-package kr.wooco.woocobe.core.place.vo
-
-data class PlaceGeoLocation(
-    val latitude: Double,
-    val longitude: Double,
-)
--- core/src/main/kotlin/kr/wooco/woocobe/core/place/vo/PlaceInfo.kt
+++
@@ -1,7 +0,0 @@
-package kr.wooco.woocobe.core.place.vo
-
-data class PlaceInfo(
-    val name: String,
-    val address: String,
-    val phoneNumber: String,
-)
