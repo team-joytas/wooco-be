1deb12c69fe1c96ec90fb2cfca108800b4a314e4
JiHongKim98 <kinjihong9598@gmail.com>
2026-02-10 18:02:43 +0900

    feat: Spring AOT 및 CDS 적용

--- bootstrap/api/build.gradle.kts
+++ bootstrap/api/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.springframework.boot.gradle.tasks.aot.ProcessAot
+import org.springframework.boot.gradle.tasks.aot.ProcessTestAot
 import org.springframework.boot.gradle.tasks.bundling.BootJar
 
 val jar: Jar by tasks
@@ -5,10 +7,10 @@ val bootJar: BootJar by tasks
 
 jar.enabled = true
 bootJar.enabled = true
-bootJar.destinationDirectory.set(rootProject.layout.buildDirectory.dir("libs"))
 
 dependencies {
     apply(plugin = "org.springframework.boot")
+    apply(plugin = "org.springframework.boot.aot")
 
     implementation(project(":core"))
     implementation(project(":support:metric"))
@@ -22,6 +24,7 @@ dependencies {
 
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
 
     runtimeOnly("io.jsonwebtoken:jjwt-impl:${property("jjwtVersion")}")
@@ -29,4 +32,53 @@ dependencies {
     implementation("io.jsonwebtoken:jjwt-api:${property("jjwtVersion")}")
 
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:${property("springDocVersion")}")
+}
+
+// AOT 설정 적용
+tasks.named<BootJar>("bootJar") {
+    dependsOn("processAot")
+
+    layered {
+        enabled = true
+    }
+
+    mainClass.set("kr.wooco.woocobe.api.WoocoBeApplicationKt")
+}
+
+tasks.named<ProcessAot>("processAot") {
+    enabled = true
+    dependsOn("classes")
+}
+
+tasks.named<ProcessTestAot>("processTestAot") {
+    enabled = false
+}
+
+// TODO-HONG: Convention plugin 고려해야함
+
+val dockerImageName: String = project.findProperty("imageName")?.toString()
+    ?: project.name.toString()
+val dockerImageVersion: String = project.findProperty("imageVersion")?.toString()
+    ?: project.version.toString()
+
+tasks.register<Exec>("buildImage") {
+    description = "Build Docker image for ${project.name}"
+
+    dependsOn("bootJar")
+
+    workingDir(projectDir)
+
+    commandLine(
+        "docker",
+        "build",
+        "-t",
+        "$dockerImageName:$dockerImageVersion",
+        "-f",
+        "Dockerfile",
+        ".",
+    )
+
+    doFirst {
+        println("Building Docker image: $dockerImageName:$dockerImageVersion")
+    }
 }
